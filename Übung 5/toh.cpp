8834ae7b527ac497969ca3d6cd4e6aee6eac43d6
Domsepp <paulpanterodt4@gmail.com>
2022-06-28 15:00:45 +0200

    First terminal output added

--- Übung 5/toh.cpp
+++ Übung 5/toh.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <tuple>
 #include <vector>
+#include <fstream>
+
 
 class TowerOfHanoi
 {
@@ -53,11 +55,44 @@ private:
 	unsigned int moves = 0;
 };
 
+std::string generate_line(int width){
+	std::string line = "  ";
+	std::cout << width << std::endl;
+	for(int i = 1; i<=21;i++){
+		if(i<=((21-width)/2) || i>=(21-((21-width)/2))){
+			line.push_back(' ');
+		}
+		else{
+			line.push_back('+');
+		}
+	}
+	line.push_back(' ');
+	return line;
+}
+
+void pretty_print(int first_pole, int second_pole, int third_pole){
+	std::vector<std::string> ouput_graphics;
+	for(int i = 0; i<first_pole;i++){
+		ouput_graphics.push_back(generate_line(((i*2)+3)));	
+	}
+	ouput_graphics.push_back(" #####################  #####################  ##################### ");
+	for(std::string &line: ouput_graphics){
+		std::cout << line << std::endl;
+	}
+}
+
 int main()
 {
-	constexpr int ItemCount = 4;
+	/*constexpr int ItemCount = 4;
 	TowerOfHanoi toh(ItemCount);
-	toh.runSimulation();
+	toh.runSimulation();*/
+	std::cout << "Press any key to continue...";
+	char ch = getchar();
+	std::cout << ch;
+	if(ch){
+		std::cout << "Congrats"<< std::endl;
+		pretty_print(5,4,5);
+	}
 
 	return 0;
 }
