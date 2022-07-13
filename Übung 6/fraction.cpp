847779166121dc9cc92e003e259d8a485cc584eb
Domsepp <paulpanterodt4@gmail.com>
2022-07-13 16:11:50 +0200

    reduce added

--- Übung 6/fraction.cpp
+++ Übung 6/fraction.cpp
@@ -1,6 +1,63 @@
 #include <cassert>
 #include <iostream>
 
+class Fraction{
+	private:
+
+	int z_;
+	int n_;
+
+	public:
+
+	Fraction(int z, int n){
+		z_ = z;
+		assert(n!=0);
+		n_ = n;
+	}
+	Fraction(int z){
+		z_ = z;
+		n_ = 1;
+	}
+
+	friend std::ostream& operator<< (std::ostream& os,Fraction& frac);
+
+	Fraction reduce(Fraction& frac){
+		bool neg = false;
+		int small, large;
+		if(frac.n_<0 != frac.z_<0){
+			neg = true;
+		}
+		frac.n_ = std::abs(frac.n_);
+		frac.z_ = std::abs(frac.z_);
+		if(frac.n_ < frac.z_){
+			small = frac.n_; large = frac.z_;
+		}
+		else{
+			small = frac.z_; large = frac.n_;
+		}
+		while(small != large){
+			large = large - small;
+			if(large < small){
+				small += large;
+				large = small - large;
+				small -= large;
+			}
+		}
+		frac.n_ /= large;
+		frac.z_ /= large;
+		if(neg) frac.n_ *= -1;
+	}
+};
+
+
+std::ostream& operator<<(std::ostream& os,Fraction& frac){
+	if(std::abs(frac.z_) < std::abs(frac.n_)){
+		os << frac.z_ << "/" << frac.n_;
+	}
+	return os;
+}
+
+
 void runTests()
 {
 	/* Test exception */
@@ -13,6 +70,9 @@ void runTests()
 	//{
 	//}
 	// std::cout << Fraction(2, 3) - Fraction(2, 3) << '\n'; // Should print 0 and not throw an exception
+	Fraction frac = Fraction(2,3);
+	
+	std::cout << frac << "\n";
 
 	/* Store values instead of generating them inline to better test the method's signature */
 	// const Fraction fraction_4_5(4, 5);
