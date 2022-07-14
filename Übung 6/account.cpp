9e63bdc1aa0f4c67a4ffbd56ceb4d49117440587
Asterohobit <tobias.rothe-gsh@gmx.de>
2022-07-14 17:38:41 +0200

    Update account.cpp
    - Initialisierung funktioniert, Vererbung auch
    - beim instanziieren der Accounts in der Schleife werden sie beim nächsten Schleifendurchlauf zerstört, sodass der Destruktor aufgerufen wird, sie sind allerdings im vector drin und können verwendet werden -> sorgt für falsche Destroyed-Ausgabe
    - youth accounts dürfen nicht ins negative fallen, habe ich implementiert aber funktioniert nicht -> code läuft als ob bedingung nicht da wäre

--- Übung 6/account.cpp
+++ Übung 6/account.cpp
@@ -1,72 +1,97 @@
 #include <iostream>
 #include <random>
 
-class Account
-{
-public:
-	explicit Account(int accountNumber)
-		: m_accountNumber(accountNumber)
-	{
+class Account {
+	protected:
+	int m_accountNumber;
+	int64_t m_balance = 0;
+
+	public:
+	explicit Account(int accountNumber) {
+		m_accountNumber = accountNumber;
 		std::cout << "Account " << m_accountNumber << ": Created\n";
 	}
 
-	int64_t balance() const
-	{
+	int64_t balance() const {
 		return m_balance;
 	}
 
-	int accountNumber() const
-	{
+	int accountNumber() const {
 		return m_accountNumber;
 	}
 
-	bool transaction(int64_t amount, const std::string& reason)
-	{
+	bool transaction(int64_t amount, const std::string& reason) {
 		std::cout << "Account " << m_accountNumber << ": Transaction (Amount: " << amount
 				  << " Reason: " << reason << ")\n";
 		m_balance += amount;
 		return true;
 	}
-
-private:
-	int m_accountNumber;
-	int64_t m_balance = 0;
 };
 
-int main()
-{
+class SavingAccount : public Account {
+	public:
+	SavingAccount(int accountNumber) : Account(accountNumber){}
+
+	~SavingAccount() { // Destruktor
+		std::cout << "Account " << m_accountNumber << ": Destroyed saving account (Balance: " << m_balance << ")\n";
+	}
+};
+
+class YouthAccount : public Account {
+	public:
+	YouthAccount(int accountNumber) : Account(accountNumber){}
+
+	~YouthAccount() { // Destruktor
+		std::cout << "Account " << m_accountNumber << ": Destroyed youth account (Balance: " << m_balance << ")\n";
+	}
+
+	bool transaction(int64_t amount, const std::string& reason) {
+		if (m_balance < -amount) {
+			std::cout << "Account " << m_accountNumber << ": Declined transaction (Current balance: " << m_balance << ", Requested Transaction: " << amount << ")\n";
+			return false;
+		}else {
+			std::cout << "Account " << m_accountNumber << ": Transaction (Amount: " << amount << " Reason: " << reason << ")\n";
+			m_balance += amount;
+		}
+		return true;
+	}
+};
+
+int main() {
 	// TODO: Adjust this type to store all account types
 	std::vector<Account> m_accounts;
 
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::bernoulli_distribution youthAccountDistrib(0.25);
-	std::uniform_int_distribution initDepositDistrib(0, 50);
+	std::bernoulli_distribution youthAccountDistrib(1);
+	std::uniform_int_distribution<> initDepositDistrib(0, 50); // ohne <> wirft es bei mir nen error: missing template arguments before 'initDepositDistrib'
 
-	constexpr int accountCount = 20;
-	for (int i = 0; i < accountCount; ++i)
-	{
-		m_accounts.push_back(Account(i)); // TODO: Remove this line and create specific account instead
-		if (youthAccountDistrib(gen))
-		{
+	constexpr int accountCount = 3; // used to be 20
+	for (int i = 0; i < accountCount; ++i) {
+		//m_accounts.push_back(Account(i)); // TODO: Remove this line and create specific account instead
+		if (youthAccountDistrib(gen)) {
 			// Create YouthAccount
-		}
-		else
-		{
+			YouthAccount temp = YouthAccount(i);
+			m_accounts.push_back(temp);
+		} else {
 			// Create SavingAccount
+			SavingAccount temp = SavingAccount(i);
+			m_accounts.push_back(temp);
 		}
 		m_accounts.back().transaction(initDepositDistrib(gen), "Deposit");
 	}
 
-	std::uniform_int_distribution transactionDistrib(-100, 100);
+	std::uniform_int_distribution<> transactionDistrib(-100, 100); //same here as in line 67
 	constexpr int simulationRuns = 5;
-	for (int i = 0; i < simulationRuns; ++i)
-	{
-		for (auto& account : m_accounts)
-		{
+	for (int i = 0; i < simulationRuns; ++i) {
+		for (auto& account : m_accounts) {
 			account.transaction(transactionDistrib(gen), "Random transaction");
 		}
 	}
 
+	// temporär, für debugging
+	for (auto& account : m_accounts) {
+		std::cout << "[DEBUG OUTPUT] Account " << account.accountNumber() << " has balance: " << account.balance() << std::endl;
+	}
 	return 0;
 }
