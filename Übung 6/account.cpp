96ad6d6fcdb7fbe3a940657fd7ada22d92915775
Asterohobit <tobias.rothe-gsh@gmx.de>
2022-07-16 23:41:23 +0200

    Update account.cpp
    - Ablehnen von Transaktionen für YouthAccounts funktioniert jetzt
    - SavingAccounts und YouthAccounts können jetzt beide in einem Pointer-Vektor gespeichert werden
    - Ich schaffe es nur, Pointer mit dem new keyword hinzuzufügen, das speichert die Accounts allerdings auf dem heap (laut stackoverflow), den muss ich selber organisieren, deswegen wird, wenn main zuende ist nicht automatisch der destruktor aufgerufen, aber ich schaffe es nicht, die accounts mittels delete selbst zu löschen, da ich über die pointer irgendwie nur noch an die variablen aber nicht mehr an den destruktor rankomme

--- Übung 6/account.cpp
+++ Übung 6/account.cpp
@@ -59,41 +59,50 @@ class YouthAccount : public Account {
 
 int main() {
 	// TODO: Adjust this type to store all account types
-	std::vector<Account> m_accounts;
+	std::vector<Account*> m_accounts;
 
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::bernoulli_distribution youthAccountDistrib(1); // used to be 0.25
+	std::bernoulli_distribution youthAccountDistrib(0.75); // used to be 0.25
 	std::uniform_int_distribution<> initDepositDistrib(0, 50); // ohne <> wirft es bei mir nen error: missing template arguments before 'initDepositDistrib'
 	int64_t init = initDepositDistrib(gen);
-	m_accounts.push_back(YouthAccount(-1));
-	constexpr int accountCount = 3; // used to be 20
+	constexpr int accountCount = 5; // used to be 20
 	for (int i = 0; i < accountCount; ++i) {
 		//m_accounts.push_back(Account(i)); // TODO: Remove this line and create specific account instead
 		if (youthAccountDistrib(gen)) {
 			// Create YouthAccount
-			YouthAccount temp = YouthAccount(i);
-			m_accounts.push_back(temp);
-			m_accounts.back().transaction(init, "Youth welcome gift");
+			//YouthAccount *temp = new YouthAccount(i);
+			//m_accounts.push_back(temp);
+			m_accounts.push_back(new YouthAccount(i));
+			m_accounts.back()->transaction(init, "Youth welcome gift");
 		} else {
 			// Create SavingAccount
-			SavingAccount temp = SavingAccount(i);
-			m_accounts.push_back(temp);
+			//SavingAccount temp = SavingAccount(i);
+			//m_accounts.push_back(&temp);
+			m_accounts.push_back(new SavingAccount(i));
 		}
-		m_accounts.back().transaction(initDepositDistrib(gen), "Deposit");
+		m_accounts.back()->transaction(initDepositDistrib(gen), "Deposit");
 	}
 
 	std::uniform_int_distribution<> transactionDistrib(-100, 100); //same here as in line 67
-	constexpr int simulationRuns = 2; // used to be 5
+	constexpr int simulationRuns = 1; // used to be 5
 	for (int i = 0; i < simulationRuns; ++i) {
-		for (auto& account : m_accounts) {
-			account.transaction(transactionDistrib(gen), "Random transaction");
+		for (auto *account : m_accounts) {
+			account->transaction(transactionDistrib(gen), "Random transaction");
 		}
 	}
 
 	// temporär, für debugging
 	for (auto& account : m_accounts) {
-		std::cout << "[DEBUG OUTPUT] Account " << account.accountNumber() << " has balance: " << account.balance() << std::endl;
+		std::cout << "[DEBUG OUTPUT] Account " << account->accountNumber() << " has balance: " << account->balance() << std::endl;
 	}
+
+	// Jetzt ist Zerstörung angesagt // welp, doesn't call the destructor tho
+	std::cout << "m_accounts.size() = " << m_accounts.size() << std::endl;
+	for (Account* account : m_accounts) {
+		delete account;
+	}
+	std::cout << "m_accounts.size() = " << m_accounts.size() << std::endl;
+	
 	return 0;
 }
