c554267baecf70713f89689278fd9b9cd667cfc7
Asterohobit <tobias.rothe-gsh@gmx.de>
2022-07-17 13:16:58 +0200

    Finished account.cpp
    - nur der Destruktor von Account musste virtual sein
    - Alle Debug-Ausgaben entfernt
    - sieht richtig aus

--- Übung 6/account.cpp
+++ Übung 6/account.cpp
@@ -11,6 +11,8 @@ class Account {
 		m_accountNumber = accountNumber;
 		std::cout << "Account " << m_accountNumber << ": Created\n";
 	}
+	
+	virtual ~Account(){}
 
 	int64_t balance() const {
 		return m_balance;
@@ -26,15 +28,13 @@ class Account {
 		m_balance += amount;
 		return true;
 	}
-
-	virtual ~Account(){}
 };
 
 class SavingAccount : public Account {
 	public:
 	SavingAccount(int accountNumber) : Account(accountNumber){}
 
-	virtual ~SavingAccount() { // Destruktor
+	~SavingAccount() { // Destruktor
 		std::cout << "Account " << m_accountNumber << ": Destroyed saving account (Balance: " << m_balance << ")\n";
 	}
 };
@@ -43,7 +43,7 @@ class YouthAccount : public Account {
 	public:
 	YouthAccount(int accountNumber) : Account(accountNumber){}
 
-	virtual ~YouthAccount() { // Destruktor
+	~YouthAccount() { // Destruktor
 		std::cout << "Account " << m_accountNumber << ": Destroyed youth account (Balance: " << m_balance << ")\n";
 	}
 
@@ -60,51 +60,35 @@ class YouthAccount : public Account {
 };
 
 int main() {
-	// TODO: Adjust this type to store all account types
 	std::vector<Account*> m_accounts;
 
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::bernoulli_distribution youthAccountDistrib(0.75); // used to be 0.25
+	std::bernoulli_distribution youthAccountDistrib(0.25);
 	std::uniform_int_distribution<> initDepositDistrib(0, 50); // ohne <> wirft es bei mir nen error: missing template arguments before 'initDepositDistrib'
 	int64_t init = initDepositDistrib(gen);
-	constexpr int accountCount = 5; // used to be 20
+	constexpr int accountCount = 20;
 	for (int i = 0; i < accountCount; ++i) {
-		//m_accounts.push_back(Account(i)); // TODO: Remove this line and create specific account instead
 		if (youthAccountDistrib(gen)) {
-			// Create YouthAccount
-			//YouthAccount *temp = new YouthAccount(i);
-			//m_accounts.push_back(temp);
 			m_accounts.push_back(new YouthAccount(i));
 			m_accounts.back()->transaction(init, "Youth welcome gift");
 		} else {
-			// Create SavingAccount
-			//SavingAccount temp = SavingAccount(i);
-			//m_accounts.push_back(&temp);
 			m_accounts.push_back(new SavingAccount(i));
 		}
 		m_accounts.back()->transaction(initDepositDistrib(gen), "Deposit");
 	}
 
 	std::uniform_int_distribution<> transactionDistrib(-100, 100); //same here as in line 67
-	constexpr int simulationRuns = 1; // used to be 5
+	constexpr int simulationRuns = 5;
 	for (int i = 0; i < simulationRuns; ++i) {
 		for (auto *account : m_accounts) {
 			account->transaction(transactionDistrib(gen), "Random transaction");
 		}
 	}
 
-	// temporär, für debugging
-	for (auto& account : m_accounts) {
-		std::cout << "[DEBUG OUTPUT] Account " << account->accountNumber() << " has balance: " << account->balance() << std::endl;
-	}
-
-	// Jetzt ist Zerstörung angesagt // welp, doesn't call the destructor tho
-	std::cout << "m_accounts.size() = " << m_accounts.size() << std::endl;
 	for (Account* account : m_accounts) {
 		delete account;
 	}
-	std::cout << "m_accounts.size() = " << m_accounts.size() << std::endl;
 	
 	return 0;
 }
