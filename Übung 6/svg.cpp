bc6d6b53bbfcba8e94ab96cf657a28c15cf2b031
Domsepp <paulpanterodt4@gmail.com>
2022-07-18 17:23:20 +0200

    it works!

--- Übung 6/svg.cpp
+++ Übung 6/svg.cpp
@@ -148,13 +148,21 @@ void writeSVGNode(std::ofstream& stream, int id, const Point& p)
 	// TODO 6.3c: Draw a circle at (p.x, p.y) and write the ID of the node inside of it
 	int x, y;
 	x = p.x * 20;
-	y = p.y * 60 + 10;
+	y = p.y * 60 + 15;
 	stream << "<circle cx=\"" << x << "\" cy=\"" << y << "\" r=\"10\" style=\"fill:white;stroke:black;stroke-width:1\" />\n";
+	int fontsize = 10;
+	stream << "<text x=\"" << x-(fontsize)/2 << "\" y=\"" << y+(fontsize)/2 << "\" style=\"font-family:Verdana;font-size=" << fontsize << ";fill:black\">" << id << "</text>\n";
 }
 
 void writeSVGEdge(std::ofstream& stream, const Point& p1, const Point& p2)
 {
 	// TODO 6.3c: Draw a line from (p1.x, p1.x) to (p2.x, p2.y)
+	int x1, y1, x2, y2;
+	x1 = p1.x * 20;
+	y1 = (p1.y * 60 +15) - 10;
+	x2 = p2.x * 20;
+	y2 = (p2.y * 60 +15) + 10;
+	stream << "<line x1=\"" << x1 << "\" y1=\"" << y1 << "\" x2=\"" << x2 << "\" y2=\"" << y2 <<"\" style=\"stroke:black;stroke-width:1\" />\n";
 }
 
 void writeSVG(const TreeNode* root, const std::string& filename)
@@ -162,8 +170,8 @@ void writeSVG(const TreeNode* root, const std::string& filename)
 	// TODO 6.3a: Write a valid svg file with the given filename which shows the given tree
 	int depth = findDepth(root);
 	int radius = 10;
-	int pWidth = pow(2,depth) * 2 * (radius+5);
-	int pHeight = 6*radius * (depth+1);
+	int pWidth = pow(2,depth + 1) * (radius+10);
+	int pHeight = 6*radius * (depth+1) - 30;
 
 	std::ofstream svgFile;
 	svgFile.open(filename);
@@ -176,25 +184,35 @@ void writeSVG(const TreeNode* root, const std::string& filename)
 	int curr_depth = 0;
 	int curr_width = pow(2,depth);
 	Point p;
+	Point p2;
+	p.x = pow(2, depth);
+	p.y = 0;
+	writeSVGNode(svgFile, root->id, p);
 	while(true){
 		if(!(std::count(traversedNodes.begin(),traversedNodes.end(),root->id))){
 			traversedNodes.push_back(root->id);
 		}
 		if(root->leftChild &&!(std::count(traversedNodes.begin(),traversedNodes.end(),root->leftChild.get()->id))){
 			root = root->leftChild.get();
+			p2.x = curr_width;
+			p2.y = curr_depth;
 			curr_depth++;
 			curr_width -= pow(2, (depth - curr_depth));
 			p.x = curr_width;
 			p.y = curr_depth;
 			writeSVGNode(svgFile, root->id, p);
+			writeSVGEdge(svgFile, p, p2);
 		}
 		else if(root->rightChild &&!(std::count(traversedNodes.begin(),traversedNodes.end(),root->rightChild.get()->id))){
 			root = root->rightChild.get();
+			p2.x = curr_width;
+			p2.y = curr_depth;
 			curr_depth++;
 			curr_width += pow(2, (depth - curr_depth));
 			p.x = curr_width;
 			p.y = curr_depth;
 			writeSVGNode(svgFile, root->id, p);
+			writeSVGEdge(svgFile, p, p2);
 		}
 		else if(root->parent){
 			root = root->parent;
@@ -225,6 +243,7 @@ int main()
 {
 	const auto root1 = newTree({ 6, 2, 1, 4, 3, 5, 7, 9, 8 });
 	const auto root2 = newTree({ 5, 9, 7, 6, 2, 1, 4, 3, 8 });
+	const auto root3 = newTree({6,2,3,8,7,5,4,1,4,3,2,9,3,10,4,4,6,1,32,4,2,1,4,3,12,3,2,34,12,4,32,8,47,667});
 /*
 	std::cout << "TreeNode 1:" << std::endl;
 	std::cout << "Breadth-first: ";
@@ -248,11 +267,11 @@ int main()
 	writeSVG(root2.get(), "tree2.svg");
 */
 	std::cout << "Depth-first:   ";
-	depthFirstTraversal(root1.get());
+	depthFirstTraversal(root3.get());
 	std::cout << "\nBreadth-first: ";
-	breadthFirstTraversal(root1.get());
+	breadthFirstTraversal(root3.get());
 	std::cout << std::endl << std::endl;
 
-	writeSVG(root1.get(),"tree.svg");
+	writeSVG(root3.get(),"tree.svg");
 	return 0;
 }
